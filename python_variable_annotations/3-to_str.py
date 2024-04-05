a0ac4d9f2bc4f823483950bcb82694922bfdf57d
xelilovkamran <xlilovkamran@gmail.com>
2024-04-05 09:26:11 +0400

    Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""A simple Python module with type annotations."""
+
+
+def to_str(n: float) -> str:
+    """Return a string representation of an integer."""
+    return str(n)
