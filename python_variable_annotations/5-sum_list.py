1c82c263d5a252d06494c5aca63269dc3db61ef8
xelilovkamran <xlilovkamran@gmail.com>
2024-04-05 09:33:14 +0400

    complex types - list of floats

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -8,11 +8,3 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """Sum the values of a list of floats."""
     return sum(input_list)
-
-
-floats = [3.14, 1.11, 2.22]
-floats_sum = sum_list(floats)
-print(floats_sum == sum(floats))
-print(sum_list.__annotations__)
-# print("sum_list(floats) returns {} which is a {}".format(
-#     floats_sum, type(floats_sum)))
