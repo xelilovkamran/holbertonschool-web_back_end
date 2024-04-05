00eb8afaf729a0948aee71943c8866b80535e364
xelilovkamran <xlilovkamran@gmail.com>
2024-04-05 09:13:05 +0400

    Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""A simple Python module with type annotations."""
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers together and return the result."""
+    return a + b
