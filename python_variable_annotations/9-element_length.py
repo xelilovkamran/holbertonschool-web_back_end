e66bcde1cacada4990b7428b748b050c8ebfe1f7
xelilovkamran <xlilovkamran@gmail.com>
2024-04-05 09:59:35 +0400

    duck type an iterable object

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """A simple Python module with type annotations."""
 
-from typing import List, Tuple
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return a list of tuples containing a string and its length."""
     return [(i, len(i)) for i in lst]
