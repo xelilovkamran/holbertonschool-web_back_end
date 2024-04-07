8d534c660c04e08bb195c9a0e148a96293bccd83
xelilovkamran <xlilovkamran@gmail.com>
2024-04-07 20:55:57 +0400

    tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""provide a function task_wait_n that takes an integer n"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return the list of all the delays in ascending order"""
+    delays: List[float] = []
+    tasks: List = []
+
+    for _ in range(n):
+        tasks.append(task_wait_random(max_delay))
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    return delays
