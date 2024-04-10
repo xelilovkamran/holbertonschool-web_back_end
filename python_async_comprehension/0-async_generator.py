4b85feda8214f6b804c9d9cfd439ce012c374378
xelilovkamran <xlilovkamran@gmail.com>
2024-04-11 01:39:34 +0400

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,17 +3,12 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator, Any
+from typing import AsyncGenerator
 
 
-async def numbers(numbers: int) -> AsyncGenerator[int, None]:
-    """Generate numbers from 0 to numbers."""
-    for i in range(numbers):
-        yield i
-
-
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Generate 10 random numbers between 0 and 10 with a 1 second delay between each number."""
-    async for i in numbers(10):
+async def async_generator() -> AsyncGenerator[int, None]:
+    """Async Generator that yields random numbers
+    between 0 and 10 every second for 10 seconds."""
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
