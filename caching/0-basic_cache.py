c8ed384d86f45751eeee007e54ff051502745759
xelilovkamran <xlilovkamran@gmail.com>
2024-04-12 16:53:24 +0400

    Basic dictionary

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Basic dictionary """
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCach(BaseCaching):
+    """ BasicCach class"""
+
+    def __init__(self):
+        """ Initiliaze """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key == None or item == None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key == None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
