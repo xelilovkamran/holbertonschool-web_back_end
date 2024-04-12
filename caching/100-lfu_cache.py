e1c7226ed88e3d754f2befcdc6518a92bfd7fbe3
xelilovkamran <xlilovkamran@gmail.com>
2024-04-12 22:19:06 +0400

    LFU Caching

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -14,25 +14,27 @@ class LFUCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key and item:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-                self.lfu_elements[key] += 1
-            elif len(self.cache_data) >= self.MAX_ITEMS:
-                frequently_used_key = list(self.cache_data.items())[0][0]
-                for cache_item in self.lfu_elements.items():
-                    if list(cache_item)[1] < self.lfu_elements[frequently_used_key]:
-                        frequently_used_key = cache_item[0]
+        if not key or not item:
+            return
 
-                self.cache_data.pop(frequently_used_key)
-                self.cache_data[key] = item
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.lfu_elements[key] += 1
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            frequently_used_key = list(self.cache_data.items())[0][0]
+            for cache_item in self.lfu_elements.items():
+                if list(cache_item)[1] < self.lfu_elements[frequently_used_key]:
+                    frequently_used_key = cache_item[0]
 
-                self.lfu_elements.pop(frequently_used_key)
-                self.lfu_elements[key] = 1
-                print("DISCARD: {}".format(frequently_used_key))
-            else:
-                self.cache_data[key] = item
-                self.lfu_elements[key] = 1
+            self.cache_data.pop(frequently_used_key)
+            self.cache_data[key] = item
+
+            self.lfu_elements.pop(frequently_used_key)
+            self.lfu_elements[key] = 1
+            print("DISCARD: {}".format(frequently_used_key))
+        else:
+            self.cache_data[key] = item
+            self.lfu_elements[key] = 1
 
     def get(self, key):
         """ Get an item by key """
