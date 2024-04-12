2ee68fff109a3e60d62abdb4cd4ddba56d5e4dda
xelilovkamran <xlilovkamran@gmail.com>
2024-04-12 17:20:53 +0400

    FIFO caching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" FIFO caching """
+
+from base_caching import BaseCaching
+
+
+class FIFOCache (BaseCaching):
+    """ LIFOCache class"""
+
+    def __init__(self):
+        """ Initiliaze """
+        super().__init__()
+        previous_added = None
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_item = list(self.cache_data.keys())[0]
+                self.cache_data.pop(first_item)
+                self.cache_data[key] = item
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if key in self.cache_data:
+            return self.cache_data[key]
