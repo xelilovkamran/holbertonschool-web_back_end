4b693a32b6c0446b5bd0d0409438d940e779e686
xelilovkamran <xlilovkamran@gmail.com>
2024-04-12 20:07:22 +0400

    MRU Caching

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -1,34 +1,24 @@
 #!/usr/bin/env python3
-""" LIFO caching """
+""" MRU caching """
 
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ LIFOCache class"""
-
-    def __init__(self):
-        """ Constructor """
-        super().__init__()
-        self.mru_keys = []
+    """ MRUCache class """
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key and item:
-            if key in self.mru_keys:
-                self.mru_keys.remove(key)
-            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discard = self.mru_keys.pop(3)
-                del self.cache_data[discard]
-                print("DISCARD: {}".format(discard))
-            self.mru_keys.append(key)
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last = self.cache_data.popitem()
+                print("DISCARD:", last[0])
             self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
         if key in self.cache_data:
             returned_value = self.cache_data[key]
-            self.mru_keys.remove(key)
-            self.mru_keys.append(key)
+            self.lru_keys.remove(key)
+            self.lru_keys.append(key)
             return returned_value
-        return None
