71c7c6e41f3170d883e0ef368582bc2c5a471d48
xelilovkamran <xlilovkamran@gmail.com>
2024-04-12 17:06:29 +0400

    basic dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -9,11 +9,10 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """ Add an item in the cache """
-        if key == None or item == None:
-            return
-        self.cache_data[key] = item
+        if key or item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key """
-        if not key == None and key in self.cache_data:
+        if key in self.cache_data:
             return self.cache_data[key]
