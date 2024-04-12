4a53941486e93521941d797309583c63db86ce5f
xelilovkamran <xlilovkamran@gmail.com>
2024-04-12 21:47:24 +0400

    LFU Caching

---
+++ caching/100-lfu_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" LIFO caching """
+
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ LIFO cache class """
+
+    def __init__(self):
+        """ Override superclass __init__ """
+        super().__init__()
+        self.lfu_elements = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.lfu_elements[key] += 1
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                frequently_used_key = list(self.cache_data.items())[0][0]
+                for cache_item in self.lfu_elements.items():
+                    if list(cache_item)[1] < self.lfu_elements[frequently_used_key]:
+                        frequently_used_key = cache_item[0]
+
+                self.cache_data.pop(frequently_used_key)
+                self.cache_data[key] = item
+
+                self.lfu_elements.pop(frequently_used_key)
+                self.lfu_elements[key] = 1
+                print("DISCARD: {}".format(frequently_used_key))
+            else:
+                self.cache_data[key] = item
+                self.lfu_elements[key] = 1
+
+    def get(self, key):
+        """ Get an item by key """
+        if key in self.cache_data:
+            self.lfu_elements[key] += 1
+            return self.cache_data[key]
