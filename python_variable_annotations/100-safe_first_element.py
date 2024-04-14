a1e9fd778ab148a61ca271ca86e269033e910924
xelilovkamran <xlilovkamran@gmail.com>
2024-04-14 12:37:25 +0400

    Duck typing - first element of a sequence

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
+"""Augment the following code with the correct duck-typed annotations:
+You need to keep the same order to pass the checks (Union[Any, None])
 """
-    Duck typing sequence Any
-"""
-from typing import Any, Sequence, Union
+from typing import Sequence, List, Tuple, Iterable, Union, Any
 
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """
-        Args:
-            lst: Any data type
-
-        Return:
-            None or first element
-    """
+    """a function
+    takes in a sequence of any type and returns the first element if it exists
+    or None if the sequence is empty. """
     if lst:
         return lst[0]
     else:
