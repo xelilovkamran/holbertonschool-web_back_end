6df559808cf620e4f84e6eb19517972afb222d02
xelilovkamran <xlilovkamran@gmail.com>
2024-04-19 01:00:20 +0400

    Regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str, seperator: str) -> str:
     """ function that returns a string with certain fields redacted """
     for field in fields:
-        regex = rf'{field}=([^;]+){seperator}'
-        message = re.sub(regex, f"{field}={redaction};", message)
+        regex = rf'{field}=([^{seperator}]+)'
+        message = re.sub(regex, f"{field}={redaction}", message)
 
     return message
