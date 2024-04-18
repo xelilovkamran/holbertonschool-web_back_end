60cf11c1d250c38d13f60f046cfcfac678736d71
xelilovkamran <xlilovkamran@gmail.com>
2024-04-19 03:10:01 +0400

    changed database connection credentials

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -55,9 +55,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     '''Returns a connector to the database'''
     connection = mysql.connector.connection.MySQLConnection(
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', 'root1234'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv("PERSONAL_DATA_DB_NAME", "my_db")
+        database=os.getenv("PERSONAL_DATA_DB_NAME")
     )
     return connection
 
