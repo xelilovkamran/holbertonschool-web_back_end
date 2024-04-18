1f073ef2f6f23dc425a5385821f3828beca62e2a
xelilovkamran <xlilovkamran@gmail.com>
2024-04-19 01:38:33 +0400

    regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,5 +9,6 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """Filtering values of the required fields"""
     for field in fields:
         message = re.sub("{}=.*?{}".format(field, separator),
-                         "{}={}{}".format(field, redaction, separator), message)
+                         "{}={}{}".format(field, redaction, separator),
+                         message)
     return message
