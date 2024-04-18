eeb1289e5045bc7897471afc388beaffac46f975
xelilovkamran <xlilovkamran@gmail.com>
2024-04-19 01:36:39 +0400

    regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,6 +8,6 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Filtering values of the required fields"""
     for field in fields:
-        message = re.sub(f"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}", message)
+        message = re.sub("{}=.*?{}".format(field, separator),
+                         "{}={}{}".format(field, redaction, separator), message)
     return message
