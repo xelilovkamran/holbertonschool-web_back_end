b49bcadb22a56bb2d9bb598cb4ceb88ff4093696
xelilovkamran <xlilovkamran@gmail.com>
2024-04-19 01:40:59 +0400

    regex-ing

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-"""filtered logger"""
+'''filtered logger'''
 
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """Filtering values of the required fields"""
-    for field in fields:
-        message = re.sub("{}=.*?{}".format(field, separator),
-                         "{}={}{}".format(field, redaction, separator),
-                         message)
+    '''Filtering values of the required fields'''
+    for item in fields:
+        message = re.sub(f"{item}=.*?{separator}",
+                         f"{item}={redaction}{separator}", message)
     return message
