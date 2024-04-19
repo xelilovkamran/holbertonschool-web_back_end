8c650680d4680cc39047aeafa7438cff3f5089f9
xelilovkamran <xlilovkamran@gmail.com>
2024-04-20 01:41:33 +0400

    Error handler: Unauthorized

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,18 +14,18 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized():
-    """ unauthorized handler
-    """
-    return Response(jsonify({"error": "Unauthorized"}), 401)
 
 
 if __name__ == "__main__":
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,6 +26,6 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route("/unauthorized/", strict_slashes=False)
+@app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
 def unauthorized_abort():
     abort(401)
