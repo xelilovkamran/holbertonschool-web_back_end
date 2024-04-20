0209321d17f4ce423fbdaac1446dd05998e546e7
xelilovkamran <xlilovkamran@gmail.com>
2024-04-20 17:48:53 +0400

    Auth class

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,12 +12,19 @@ class Auth:
     def __init__(self):
         """ Constructor of the Auth class
         """
-        
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth method that returns
+        True if the path is not in the list
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ authorization_header method that returns None
+        """
         return request
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user method that returns None
+        """
         return request
