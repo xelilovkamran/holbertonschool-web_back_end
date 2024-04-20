003fdea399dd2046d35b19658a897405c3316b15
xelilovkamran <xlilovkamran@gmail.com>
2024-04-20 21:31:34 +0400

    Basic - Base64 part

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,3 +12,14 @@ class BasicAuth(Auth):
     def __init__(self):
         """ Constructor of the BasicAuth class
         """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract_base64_authorization_header method that returns None
+        """
+        if (authorization_header is None or
+            type(authorization_header) is not str or
+                authorization_header.startswith("Basic ") is False):
+            return None
+
+        return authorization_header[6:]
