8c4f9c5fd66dcf9c7684c6e53c91577fe86a74f8
xelilovkamran <xlilovkamran@gmail.com>
2024-04-20 17:39:45 +0400

    Auth class

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """ Auth module
 """
 
@@ -10,11 +9,15 @@ from typing import List, TypeVar
 class Auth:
     """ Auth class for the API
     """
+    def __init__(self) -> None:
+        """ Constructor of the Auth class
+        """
+        
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         return False
 
     def authorization_header(self, request=None) -> str:
-        return None
+        return request
 
     def current_user(self, request=None) -> TypeVar('User'):
-        return None
+        return request
