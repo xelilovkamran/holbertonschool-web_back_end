ba74f771ff2155d46f14b3182828aca527dffa53
xelilovkamran <xlilovkamran@gmail.com>
2024-04-20 17:21:01 +0400

    Error handler: Forbidden

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -27,12 +27,12 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
-    
 
 
 if __name__ == "__main__":
