8250ee69fdbff029bedf974ae84ed1152a55fc58
xelilovkamran <xlilovkamran@gmail.com>
2024-04-20 21:27:15 +0400

    Basic auth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,13 @@
 """ BasicAuth module for the API
 """
 
-from auth import Auth
+from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ BasicAuth class
     """
+
+    def __init__(self):
+        """ Constructor of the BasicAuth class
+        """
