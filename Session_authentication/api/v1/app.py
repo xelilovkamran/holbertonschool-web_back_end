1cac751281acc8de0ec30f9a1f735a45a7d9f7cf
xelilovkamran <xlilovkamran@gmail.com>
2024-04-21 15:13:32 +0400

    Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -66,10 +66,10 @@ def before_request() -> str:
     if (auth.authorization_header(request) is None and
             auth.session_cookie(request) is None):
         abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    # if auth.current_user(request) is None:
+    abort(403)
 
-    request.current_user = auth.current_user(request)
+    # request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
