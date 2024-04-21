c7dca0c9ebb0ce36e5b8aba63bbc1be4c8f58cb4
xelilovkamran <xlilovkamran@gmail.com>
2024-04-21 23:42:39 +0400

    Sessions in database

---
+++ Session_authentication/models/user_session.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" UserSession module
+"""
+
+from models.base import Base
+
+
+class UserSession(Base):
+    """ UserSession class
+    """
+
+    def __init__(self, *args: list, **kwargs: dict):
+        """ initialize user session
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id: str = kwargs.get('user_id')
+        self.session_id: str = kwargs.get('session_id')
