34f009b8e1063a8f7a9231ad2b03e4473d407264
xelilovkamran <xlilovkamran@gmail.com>
2024-04-21 15:52:46 +0400

    new view for Session Authentication

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -12,25 +12,25 @@ def login():
     email: str = request.form.get("email")
 
     if not email:
-        return make_response(jsonify({"error": "email missing"}, 400))
+        return make_response(jsonify({"error": "email missing"}), 400)
 
     pwd: str = request.form.get("password")
 
     if not pwd:
-        return make_response(jsonify({"error": "password missing"}, 400))
+        return make_response(jsonify({"error": "password missing"}), 400)
 
     users = User.search({"email": email})
 
     if len(users) == 0:
-        return jsonify({"error": "no user found for this email"}, 400)
+        return jsonify({"error": "no user found for this email"}), 400
 
     from api.v1.app import auth
     for user in users:
         if user.is_valid_password(pwd):
             session_id = auth.create_session(user.id)
-            SESSION_NAME = getenv('SESSION_NAME')
+            SESSION_NAME = getenv('SESSION_NAME', "_my_session_id")
             response = make_response(user.to_json())
             response.set_cookie(SESSION_NAME, session_id)
             return response
 
-    return make_response(jsonify({"error": "wrong password"}, 401))
+    return make_response(jsonify({"error": "wrong password"}), 401)
