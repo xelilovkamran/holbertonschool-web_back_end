27ddc38b79263d68c9b852c9f6a3c590adc80e9f
xelilovkamran <xlilovkamran@gmail.com>
2024-04-21 14:57:20 +0400

    Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -56,15 +56,17 @@ def before_request() -> str:
 
     pathes = ['/api/v1/status/',
               '/api/v1/unauthorized/',
-              '/api/v1/forbidden/'
+              '/api/v1/forbidden/',
+              "/api/v1/auth_session/login/"
               ]
 
     if not (auth.require_auth(request.path, pathes)):
         return
 
-    if (auth.authorization_header(request)) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
-    if (auth.current_user(request)) is None:
+    if auth.current_user(request) is None:
         abort(403)
 
     request.current_user = auth.current_user(request)
