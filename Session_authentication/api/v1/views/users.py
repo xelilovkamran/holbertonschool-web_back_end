eaa6cf366ec5bcf58a4062eb951b718a7a12799b
xelilovkamran <xlilovkamran@gmail.com>
2024-04-21 14:00:29 +0400

    Et moi et moi et moi!

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -31,6 +31,7 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == "me" and request.current_user is None:
         abort(404)
 
+    user: TypeVar('User') = User()
     if user_id == "me" and request.current_user is not None:
         return jsonify(request.current_user.to_json())
 
