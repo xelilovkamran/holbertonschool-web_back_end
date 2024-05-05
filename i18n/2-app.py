464b7f9b75f3fa9ffde99ff02377c1b83e047602
xelilovkamran <xlilovkamran@gmail.com>
2024-05-05 19:04:20 +0400

    Get locale from request

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -24,6 +24,8 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """Get locale from request
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
