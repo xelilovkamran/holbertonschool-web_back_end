b83a02be2fb2059818404ab22e4300e1c35fc626
xelilovkamran <xlilovkamran@gmail.com>
2024-05-26 23:47:34 +0400

    task 0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-export function taskFirst() {
-  const task = 'I prefer const when I can.';
-  return task;
-}
-
-export function getLast() {
-  return ' is okay';
-}
-
-export function taskNext() {
-  let combination = 'But sometimes let';
-  combination += getLast();
-  return combination;
-}
-=======
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -29,4 +13,3 @@ export function taskNext() {
 
   return combination;
 }
->>>>>>> 9aedb11837d40928fa6eed4bc15ac60a401b1854
