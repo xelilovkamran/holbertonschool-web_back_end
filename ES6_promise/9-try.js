e5b47741f7fa453853a76482e194aea619a43465
xelilovkamran <xlilovkamran@gmail.com>
2024-06-02 20:02:32 +0400

    task 9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,18 +1,18 @@
 // Queue push
 function guardrail(mathFunction) {
-    const queue = [];
-    let result;
+  const queue = [];
+  let result;
 
-    try {
-        result = mathFunction();
-    } catch (e) {
-        result = e.toString();
-    }
+  try {
+    result = mathFunction();
+  } catch (e) {
+    result = e.toString();
+  }
 
-    queue.push(result);
-    queue.push('Guardrail was processed');
+  queue.push(result);
+  queue.push('Guardrail was processed');
 
-    return queue;
+  return queue;
 }
 
 export default guardrail;
