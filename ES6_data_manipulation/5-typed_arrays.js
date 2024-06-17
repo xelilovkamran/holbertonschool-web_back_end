0af607fcdeabe48bdce4b5eee5ad612fb273800e
xelilovkamran <xlilovkamran@gmail.com>
2024-06-17 08:50:43 +0400

    data manupulation final

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,11 @@
 const createInt8TypedArray = (length, position, value) => {
-  if (position >= length || position < 0) {
+  const buffer = new ArrayBuffer(length);
+  const data = new DataView(buffer);
+  if (position > length - 1) {
     throw new Error('Position outside range');
   }
-  const int8 = new Int8Array(length);
-  int8[position] = value;
-
-  const { buffer } = int8;
-  const newView = new DataView(buffer, 0, length);
-
-  return newView;
+  data.setInt8(position, value);
+  return data;
 };
 
 export default createInt8TypedArray;
--- ES6_data_manipulation/README.md
+++ ES6_data_manipulation/README.md
@@ -1,3 +1,8 @@
-# 0x11. ES6 data manipulation
- 
-## Specializations - Web Stack programming ― Back-end
+# ES6 data manipulation
+
+## Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+-  How to use map, filter and reduce on arrays
+-  Typed arrays
+-  The Set, Map, and Weak link data structures
